80b697f0d8b6313622cb7e9460b0f039fb3be66c
Илья <149706253+4URKa0@users.noreply.github.com>
2024-05-21 20:28:11 +0300

    загрузка домашки

---
+++ appModules/http-utils/index.js
@@ -0,0 +1,9 @@
+const mimeTypes = require("./mime-tyeps");
+const staticFile = require("./static-file");
+const parseBody = require("./parse-body");
+
+module.exports = {
+    mimeTypes,
+    staticFile,
+    parseBody,
+};
